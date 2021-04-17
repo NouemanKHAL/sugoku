f151ee4b415246efa49be437c4ecb83b2ed47c39
NouemanKHAL <noueman.khal@gmail.com>
2021-04-18 01:34:20 +0200

    added getters and setters

--- sudoku/sudoku.go
+++ sudoku/sudoku.go
@@ -1,24 +1,54 @@
 package sudoku
 
+import (
+	"errors"
+	"fmt"
+)
+
 const (
 	EMPTY_CELL rune = '.'
 )
 
 type sudokuGrid struct {
-	Grid [][]rune
+	Grid        [][]rune
+	IsGenerated [][]bool
+	validRows   []bool
+	validCols   []bool
 }
 
 // New Returns an empty 9 x 9 Grid
 func New() *sudokuGrid {
 	sG := sudokuGrid{}
 	sG.Grid = make([][]rune, 9)
+	sG.IsGenerated = make([][]bool, 9)
 	for i := 0; i < 9; i++ {
 		sG.Grid[i] = make([]rune, 9)
+		sG.IsGenerated[i] = make([]bool, 9)
 		for j := 0; j < 9; j++ {
 			sG.Grid[i][j] = EMPTY_CELL
 		}
 	}
 	return &sG
+}
+
+// Get returns the value of the cell with coordinates (x, y)
+func (sG *sudokuGrid) Get(x, y int) (rune, error) {
+	if x < 0 || x > 8 || y < 0 || y > 8 {
+		return EMPTY_CELL, errors.New(fmt.Sprintf("Cell coordinates out of bounds. (%d, %d)", x, y))
+	}
+	return sG.Grid[x][y], nil
+}
+
+// Set sets the value of the cell with coordinates (x, y)
+func (sG *sudokuGrid) Set(x, y int, val rune) error {
+	if x < 0 || x > 8 || y < 0 || y > 8 {
+		return errors.New(fmt.Sprintf("Cell coordinates out of bounds. (%d, %d)", x, y))
+	}
+	if sG.IsGenerated[x][y] {
+		return errors.New(fmt.Sprintf("Cannot Set generated cell (%d, %d)", x, y))
+	}
+	sG.Grid[x][y] = val
+	return nil
 }
 
 func (sG *sudokuGrid) IsValid() bool {
